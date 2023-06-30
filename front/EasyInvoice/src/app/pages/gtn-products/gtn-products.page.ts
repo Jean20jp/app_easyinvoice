1029cc3f4a25e14a20b0c3be69f0636bbb03f15e
jean96gr <jean96gr@gmail.com>
2023-06-30 16:44:58 -0500

    #4 Diseño pagina de productos

--- front/EasyInvoice/src/app/pages/gtn-products/gtn-products.page.ts
+++ front/EasyInvoice/src/app/pages/gtn-products/gtn-products.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-gtn-products',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GtnProductsPage implements OnInit {
 
-  constructor() { }
+  items: any[];
+
+  constructor(private toastController: ToastController, private http: HttpClient) {
+
+    this.items = [];
+
+   }
 
   ngOnInit() {
+  }
+
+  loadDataList() {
+    
+  }
+
+  getItems(): Observable<any[]> {
+    const url = "";
+    return this.http.get<any[]>(url);
+  }
+
+
+  eliminarItem(item: any) {
+
+  }
+
+  editarItem(item: any) {
+
+  }
+
+  mostrarDetalle(item: any) {
+
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
