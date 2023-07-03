89590e3302b222a3a3320b706aa9a54a03ff817e
jean96gr <jean96gr@gmail.com>
2023-07-02 19:44:25 -0500

    #4 Función para editar, eliminar, agregar y mostrar detalle producto

--- front/EasyInvoice/src/app/pages/gtn-products/gtn-products.page.ts
+++ front/EasyInvoice/src/app/pages/gtn-products/gtn-products.page.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { ToastController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { AlertController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-gtn-products',
@@ -11,18 +12,48 @@ import { AlertController } from '@ionic/angular';
 })
 export class GtnProductsPage implements OnInit {
 
+  codigo_barras!: string;
+  nom_producto!: string;
+  descrip_prod!: string;
+  id_prom_pro!: string;
+  id_cat_pro!: string;
+  unidades_dispon!: string;
+  precio!: string;
+
+  showBackdrop: boolean = false;
+  
   items: any[] = [
     { id: 1, name: 'Item 1', description: 'Descripción del Item 1' },
     { id: 2, name: 'Item 2', description: 'Descripción del Item 2' },
     { id: 3, name: 'Item 3', description: 'Descripción del Item 3' },
   ];
 
+  selectedItems: any[] = [];
+
   constructor(private toastController: ToastController, private http: HttpClient,
-    private alertController: AlertController) {
+    private alertController: AlertController, private modalController: ModalController) {
 
     //this.items = [];
 
    }
+
+   selectItem(item: any) {
+    const index = this.selectedItems.indexOf(item);
+    if (index > -1) {
+      this.selectedItems.splice(index, 1); // Deseleccionar elemento si ya está seleccionado
+    } else {
+      this.selectedItems.push(item); // Seleccionar elemento si no está seleccionado
+    }
+  }
+
+  isSelected(item: any): boolean {
+    return this.selectedItems.indexOf(item) > -1;
+  }
+
+  vender() {
+    // Aquí puedes capturar la información de los elementos seleccionados y realizar las acciones necesarias
+    console.log(this.selectedItems);
+  }
 
   ngOnInit() {
   }
@@ -45,14 +76,8 @@ export class GtnProductsPage implements OnInit {
 
   }
 
-  async showDetail(item: any) {
-    const alert = await this.alertController.create({
-      header: 'Detalles',
-      message: `Nombre: ${item.name}<br>Descripción: ${item.description}`,
-      buttons: ['Aceptar'],
-    });
-
-    await alert.present();
+  toggleBackdrop() {
+    this.showBackdrop = !this.showBackdrop;
   }
 
   async presentToast(message: string) {
