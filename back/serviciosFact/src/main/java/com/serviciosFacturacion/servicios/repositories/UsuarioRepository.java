ee09e2afd23393fc69cc8c9e9b7308a1f7b6502c
YomalD12 <yomald1212@gmail.com>
2023-07-03 18:51:47 -0500

    #5 Creacion de model, controller y repository de los usuarios

---
+++ back/serviciosFact/src/main/java/com/serviciosFacturacion/servicios/repositories/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.serviciosFacturacion.servicios.repositories;
+
+import com.serviciosFacturacion.servicios.models.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+
+        UsuarioModel findByNomb_usuarioAndContrasenia(String nombUsuario, String contrasenia);
+
+
+}
