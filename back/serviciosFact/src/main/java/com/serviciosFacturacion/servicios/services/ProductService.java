d4cda1bd69279e86582a7083f819d25c25961e7d
kevin <kevin@KEVIN>
2023-07-04 09:37:52 -0500

    #13 Creacion del servicio obtenerProductos

--- back/serviciosFact/src/main/java/com/serviciosFacturacion/servicios/services/ProductService.java
+++ back/serviciosFact/src/main/java/com/serviciosFacturacion/servicios/services/ProductService.java
@@ -1,4 +1,20 @@
 package com.serviciosFacturacion.servicios.services;
 
+import com.serviciosFacturacion.servicios.models.IvaModel;
+import com.serviciosFacturacion.servicios.models.ProductModel;
+import com.serviciosFacturacion.servicios.repositories.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class ProductService {
+    @Autowired
+    IProductRepository productRepository;
+
+    public ArrayList<ProductModel> getProducts(){
+        return (ArrayList<ProductModel>) productRepository.findAll();
+    }
+    
 }
