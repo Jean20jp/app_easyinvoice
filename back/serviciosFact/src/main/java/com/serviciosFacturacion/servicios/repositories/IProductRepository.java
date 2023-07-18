3316696015309127d2a2e38277d0e10b3cdd03d9
Jean Pierre Girón Rodríguez <jean96gr@gmail.com>
2023-07-18 02:32:29 +0000

    Revert "Merge branch 'feature-gtnestablecimientos' into 'main'"
    This reverts merge request !22

--- back/serviciosFact/src/main/java/com/serviciosFacturacion/servicios/repositories/IProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.serviciosFacturacion.servicios.repositories;
-
-
-import com.serviciosFacturacion.servicios.models.ProductModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IProductRepository extends JpaRepository<ProductModel,Long > {
-}
